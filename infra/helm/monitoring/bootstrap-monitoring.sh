b917126d5bc8ded74af74c4e7b4941276529d222
antoine <antoine@kwb.de>
2024-10-22 09:57:32 +0000

    smaller retention for prometheus

--- infra/helm/monitoring/bootstrap-monitoring.sh
+++ infra/helm/monitoring/bootstrap-monitoring.sh
@@ -5,11 +5,8 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 helm install prometheus prometheus-community/prometheus -n monitoring --create-namespace \
-    --set server.global.scrape_interval=20s
+    --set server.global.scrape_interval=30s,server.retention=5d
 helm install grafana grafana/grafana -n monitoring \
     --set persistence.storageClassName=microk8s-hostpath
 helm upgrade --install loki grafana/loki-stack -n monitoring \
     --set fluent-bit.enabled=false,promtail.enabled=true,grafana.enabled=false,loki.image.tag=2.9.3
-
-
-
