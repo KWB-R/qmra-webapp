0baefdf94aa7c0ac21bf0bf4b7d9ce887463a98b
adaurat <antoine.daurat@kompetenz-wasser.de>
2024-12-19 06:05:03 +0100

    update tour text, add welcome text and update refs links

--- qmra/settings.py
+++ qmra/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.getenv("STATIC_ROOT", "/var/cache/qmra/static")
+STATIC_ROOT = os.getenv("STATIC_ROOT", ".static")
 STATICFILES_DIRS = [
     BASE_DIR / "qmra/static"
 ]
